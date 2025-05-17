c60c1bf719342c350920cd873377235921e310a1
Serghei Priseajniuc <priseajniuc20000@gmail.com>
2025-05-17 13:16:21 +0300

    created project

---
+++ Space_test/Space_test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Space_test
+//
+//  Created by M1 Pro on 17.05.2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
